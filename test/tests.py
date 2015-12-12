93f64ad2585d545f3a28747691a9de1e3b8db46f
Your Name <you@example.com>
2015-12-11 23:17:54 -0800

    add a couple of checks in status unit test

--- test/tests.py
+++ test/tests.py
@@ -13,8 +13,14 @@ class TestZequsAPI(unittest.TestCase):
     def test_queryprinter(self):
         url = "http://%s:%d/api/v1/zebrabadgeprinter/" % (host, port)
         r = requests.get(url)
-        self.assertEqual(r.status_code == 200)
-        #x = r.json()
+        self.assertTrue(r.status_code == 200)
+        x = r.json()
+        self.assertTrue("status" in x)
+        self.assertTrue("jobs" in x)
+        self.assertTrue("queued" in x)
+        self.assertTrue("printing" in x)
+        self.assertTrue("failed" in x)
+        self.assertTrue("complete" in x)
 
 if __name__ == '__main__':
     unittest.main()

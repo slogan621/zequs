86adaa05cf9f6b106cba8bd42bbaa1d2938c773f
Syd Logan <slogan621@gmail.com>
2015-12-19 20:44:26 -0800

    clean up some comments and add example /etc/zequs.conf file

--- plugins/zebra-card-api.py
+++ plugins/zebra-card-api.py
@@ -53,16 +53,3 @@ class PrinterPlugin(ZequsPluginBase):
 
     def getSettings(self, printerID):
         raise NotImplementedError
-
-'''
- discover - Discover printers
- graphic  - Converts a graphic to a format that can be printed by a Zebra Card
-Printer
- help     - Get information about other commands
- position - Positions a card int the printer
- print    - Prints bitmap image files
- reset    - Resets a printer
- settings - Sets or gets the printer's settings
- status   - Gets the specified status from the printer
- update   - Updates the firmware on the printer
-''' 
--- plugins/zequspluginbase.py
+++ plugins/zequspluginbase.py
@@ -51,16 +51,3 @@ class ZequsPluginBase(object):
 
     def getSettings(self, printerID):
         raise NotImplementedError
-
-'''
- discover - Discover printers
- graphic  - Converts a graphic to a format that can be printed by a Zebra Card
-Printer
- help     - Get information about other commands
- position - Positions a card int the printer
- print    - Prints bitmap image files
- reset    - Resets a printer
- settings - Sets or gets the printer's settings
- status   - Gets the specified status from the printer
- update   - Updates the firmware on the printer
-''' 
